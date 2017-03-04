3a7f6c889281aa05e331b15bb868399912891a3a
Anton <Anton_fast@hotmail.com>
2017-03-04 12:40:30 +0100

    reastllloasdadwa

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://www.u3423643.fsdata.se/laravel/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
